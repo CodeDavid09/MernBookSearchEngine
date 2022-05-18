26294933510da939f7dc56398cb9afafc33ff817
David Gavino <ggavino2@uccs.edu>
2022-05-18 08:42:14 -0600

    modified server

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
-import {useMutation, useQuery} from '@apollo/react-hooks';
+import {useMutation, useQuery} from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
 
 const SavedBooks = () => {
 
-  const { loading, data} = useQuery(GET_ME);
   const [removeBook, {error}] = useMutation(REMOVE_BOOK);
+  const { loading, data} = useQuery(GET_ME);
 
   const userData = data?.me || [];
 
